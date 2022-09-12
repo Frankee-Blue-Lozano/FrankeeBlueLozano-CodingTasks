b3baf2288b13047516ac23c99fc14c6bdf6bd22a
Frankee-Blue-Lozano <91489835+Frankee-Blue-Lozano@users.noreply.github.com>
2022-09-12 15:14:00 -0700

    coding tasks

--- Program.cs
+++ Program.cs
@@ -6,30 +6,42 @@ namespace FrankeeBlueLozano_CodingTasks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Challenge 1:" + Sum(1, 5));
-            Console.WriteLine("Challenge 2:" + Convert(5));
-            Console.WriteLine("Challenge 3:" + PlusOne(3));
-            Console.WriteLine("Challenge 4:" + CircutPower(230, 10));
-            Console.WriteLine("Challenge 5:" + CalcAge(65, 365));
-            Console.WriteLine("Challenge 6:" + lessThanOrEqualToZero(5, 0));
-            Console.WriteLine("Challenge 7:" + triArea(3, 2));
-            Console.WriteLine("Challenge 8:" + lessThen100(22, 15));
-            Console.WriteLine("Challenge 9.1:" + IsEqual(5, 6));
-            Console.WriteLine("Challenge 9.2:" + IsEqual(1, 1));
-            Console.WriteLine("Challenge 10:" + GiveMeSomething(" is better than nothing"));
-            Console.WriteLine("Challenge 10.1:" + GiveMeSomething(" Bob Jane"));
-            Console.WriteLine("Challenge 10.2:" + GiveMeSomething(" something"));
-            Console.WriteLine("Challenge 11:" + Reverse(true));
-            Console.WriteLine("Challenge 11.1:" + Reverse(false));
-            Console.WriteLine("Challenge 12:" + howManySeconds(2));
-            Console.WriteLine("Challenge 13:" + SumPolygon(3));
-            Console.WriteLine("Challenge 13.1:" + SumPolygon(4));
-            Console.WriteLine("Challenge 13.2:" + SumPolygon(6));
-            Console.WriteLine("Challenge 14:" + nameString("Mubashir"));
-            Console.WriteLine("Challenge 14.1:" + nameString("Edabit"));
-            Console.WriteLine("Challenge 14.2:" + nameString("C#"));
-
-
+            Console.WriteLine("Challenge 1: " + Sum(1, 5));
+            Console.WriteLine("Challenge 2: " + Convert(5));
+            Console.WriteLine("Challenge 3: " + PlusOne(3));
+            Console.WriteLine("Challenge 4: " + CircutPower(230, 10));
+            Console.WriteLine("Challenge 5: " + CalcAge(65, 365));
+            Console.WriteLine("Challenge 6: " + lessThanOrEqualToZero(5, 0));
+            Console.WriteLine("Challenge 7: " + triArea(3, 2));
+            Console.WriteLine("Challenge 8: " + lessThen100(22, 15));
+            Console.WriteLine("Challenge 9.1: " + IsEqual(5, 6));
+            Console.WriteLine("Challenge 9.2: " + IsEqual(1, 1));
+            Console.WriteLine("Challenge 10: " + GiveMeSomething(" is better than nothing"));
+            Console.WriteLine("Challenge 10.1: " + GiveMeSomething(" Bob Jane"));
+            Console.WriteLine("Challenge 10.2: " + GiveMeSomething(" something"));
+            Console.WriteLine("Challenge 11: " + Reverse(true));
+            Console.WriteLine("Challenge 11.1: " + Reverse(false));
+            Console.WriteLine("Challenge 12: " + howManySeconds(2));
+            Console.WriteLine("Challenge 13: " + SumPolygon(3));
+            Console.WriteLine("Challenge 13.1: " + SumPolygon(4));
+            Console.WriteLine("Challenge 13.2: " + SumPolygon(6));
+            Console.WriteLine("Challenge 14: " + nameString("Mubashir"));
+            Console.WriteLine("Challenge 14.1: " + nameString("Edabit"));
+            Console.WriteLine("Challenge 14.2: " + nameString("C#"));
+            Console.WriteLine("Challenge 15: " + And(true, false));
+            Console.WriteLine("Challenge 15.1: " + And(true, true));
+            Console.WriteLine("Challenge 15.2: " + And(false, true));
+            Console.WriteLine("Challenge 15.3: " + And(false, false));
+            Console.WriteLine("Challenge 16: " + points(1, 1));
+            Console.WriteLine("Challenge 16.1: " + points(7, 5));
+            Console.WriteLine("Challenge 17: " + FindPerimeter(6, 7));
+            Console.WriteLine("Challenge 17.1: " + FindPerimeter(20, 10));
+            Console.WriteLine("Challenge 18: " + HelloName("Gerald"));
+            Console.WriteLine("Challenge 18.1: " + HelloName("Tiffany"));
+            Console.WriteLine("Challenge 19: " + FarmAnimals(2, 3, 5));
+            Console.WriteLine("Challenge 19.1: " + FarmAnimals(1, 2, 3));
+            Console.WriteLine("Challenge 20: " + FootballPoints(3, 4, 2));
+            Console.WriteLine("Challenge 20.1: " + FootballPoints(5, 0, 2));
         }
 
         public static int Sum(int a, int b)
@@ -109,6 +121,39 @@ namespace FrankeeBlueLozano_CodingTasks
         public static string nameString(string a)
         {
             return a + "Edabit";
+        }
+        
+        public static bool And(bool a, bool b)
+        {
+            if (a && b)
+                return true;
+            else
+                return false;
+        }
+
+        public static int points(int two, int three)
+        {
+            return (two * 2) + (three * 3);
+        }
+
+        public static int FindPerimeter(int L, int W)
+        {
+            return (L * 2) + (W * 2);
+        }
+
+        public static string HelloName(string a)
+        {
+            return "Hello " + a + "!";
+        }
+
+        public static int FarmAnimals(int chicks, int cow, int pig)
+        {
+            return (pig * 4) + (cow * 4) + (chicks * 2);
+        }
+
+        public static int FootballPoints(int wins, int draws, int losses)
+        {
+            return (wins * 3) + (draws * 1) + (losses * 0); 
         }
     }
 }
